7da4d06102010b4429685a48bbc6e21f40aecee3
mkaraki <26180919+mkaraki@users.noreply.github.com>
2024-01-20 15:56:52 +0900

    Fix psalm error

---
+++ _config.dist.php
@@ -0,0 +1,8 @@
+<?php
+const DB_CONSTR = 'host=postgres port=5432 dbname=ipdb user=ipdb password=ipdb';
+define('USER_ATK_REPORTER', [
+    'example' => 'INVALID HASH',
+]);
+define('USER_ATK_MANAGER', [
+    'admin' => 'INVALID HASH',
+]);
