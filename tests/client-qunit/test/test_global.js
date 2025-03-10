7a124946e29264952126e1cc819eeca01a443aa7
mkaraki <26180919+mkaraki@users.noreply.github.com>
2025-03-11 08:03:22 +0900

    Remove unused qunit

--- tests/client-qunit/test/test_global.js
+++
@@ -1,9 +0,0 @@
-const { nanoToSeconds } = require('../../../wwwroot/scripts/global.js');
-
-QUnit.module('global.js');
-
-QUnit.test('nanoToSeconds', function (assert) {
-    const actual = nanoToSeconds(1705477902827283177);
-    const expected = 1705477902;
-    assert.equal(actual, expected);
-});
